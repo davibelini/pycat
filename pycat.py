6a6764136722c2fc1558b70f482d19c8c4cd9cac
davibelini <dev.dado.belini@gmail.com>
2020-12-09 14:06:58 -0300

    implemented hability to read file('-f' option) and tests

--- pycat.py
+++ pycat.py
@@ -3,4 +3,15 @@ import time
 
 time.sleep(1)
 
-print(sys.argv)
+if len(sys.argv) > 2:
+  if sys.argv[1] == '-f':
+    try:
+      with open(sys.argv[2]) as file:
+        print(file.read(), end="")
+    except:
+      print("ERROR: couldn't read file")
+  elif sys.argv[1] == '-w': # Write
+    pass
+  # Include more options
+else:
+  print('ERROR: many few args')
