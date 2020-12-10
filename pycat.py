109dbd983a3c783bca13ea5993338eddc699d8c0
davibelini <dev.dado.belini@gmail.com>
2020-12-09 21:26:42 -0300

    quote_parser() function and --write option

--- pycat.py
+++ pycat.py
@@ -3,15 +3,24 @@ import time
 
 time.sleep(1)
 
+def parse_quotes(string):
+  return string.replace("\"", "")
+
+
 if len(sys.argv) > 2:
-  if sys.argv[1] == '-r':
+  option = sys.argv[1]
+  if option == '-r' or option == '--read':
     try:
       with open(sys.argv[2]) as file:
         print(file.read(), end="")
     except:
       print("ERROR: couldn't read file")
-  elif sys.argv[1] == '-w': # Write
-    pass
+  elif option == '-w' or '--write': # Write
+    new_file = open(parse_quotes(sys.argv[2]), "w")
+    new_file.write(sys.argv[3])
+    new_file.close()
   # Include more options
 else:
   print('ERROR: many few args')
+
+# TODO: parse "" characters for text
