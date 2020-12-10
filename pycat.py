3ba5698e069bd6e37e658ab8e256c85b922d19bb
davibelini <dev.dado.belini@gmail.com>
2020-12-10 19:05:11 -0300

    created binary file of program with pyinstaller

--- pycat.py
+++ pycat.py
@@ -7,18 +7,21 @@ def parse_quotes(string):
   return string.replace("\"", "")
 
 if len(sys.argv) > 2:
-  option = sys.argv[1]
-  if option == '-r' or option == '--read':
+  command = sys.argv[1]
+  if command == 'read':
     try:
       with open(sys.argv[2]) as file:
         print(file.read(), end="")
-    except:
+    except EnvironmentError as err:
       print("ERROR: couldn't read file")
-  elif option == '-c' or '--concat': # Write
+      print(err)
+  elif command == 'concat': # Write
     file_to_be_concatenated = open(sys.argv[2], "a")
     file_to_be_concatenated.write(parse_quotes(sys.argv[3]))
     file_to_be_concatenated.close()
-  # Include more options
+  elif command == 'make':
+    pass
+  # Include more options (INCLUDE -m OR --make TO CREATE FILES)
 else:
   print('ERROR: many few args')
 
