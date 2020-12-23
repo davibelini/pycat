ec0d3e1996c87c42a5600d84e0f88d8f4bb61e9a
davibelini <dev.dado.belini@gmail.com>
2020-12-23 17:35:59 -0300

    'join' option and started docs

---
+++ docs.md
@@ -0,0 +1,16 @@
+# Pycat is a redesigned version of Unix systems 'cat' command, written in python.
+# Github repository: <https://github.com/davibelini/pycat>
+
+> ## How to obtain pycat:
+- From Github:
+    - Open your terminal.
+    - Clone the repository:
+    ```sh
+    git clone https://github.com/davibelini/pycat.git/
+    ```
+    - Navigate to the repository:
+    ```sh
+    cd pycat
+    ```
+- From the website:
+    - Not yet available.
--- pycat.py
+++ pycat.py
@@ -14,7 +14,7 @@ if len(sys.argv) > 1:
         print(file.read(), end="")
     except EnvironmentError as err:
       print("ERROR: couldn't read file")
-  elif command == 'concat': # Write
+  elif command == 'write': # Write
     file_to_be_concatenated = open(sys.argv[2], "a")
     file_to_be_concatenated.write(parse_quotes(sys.argv[3]))
     file_to_be_concatenated.close()
@@ -24,15 +24,17 @@ if len(sys.argv) > 1:
       f.close()
     except:
       print("ERROR: Could not create file.")
-  elif command == 'help' or command == '?' or command == "getdocs": 
+  elif command == 'help' or command == '?' or command == "docs": 
     try:
       docs_file = open("docs.txt", "r")
-      print(docs_file.read())
+      print(docs_file.read(), end="")
     except:
       print("ERROR: could not print docs file.")
     finally:
       docs_file.close()
-  # Include more options
+  elif command == 'join':
+    with open(sys.argv[2], "a") as f:
+      f.write(open(sys.argv[3], "r").read())
   else:
     print(f"ERROR: option '{sys.argv[1]}' does not exist.")
 else:
